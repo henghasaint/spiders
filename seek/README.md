f87804f48a4bfc5bfc4abae2c36e35c0b50b0ed5
henghasaint <mercuryerflyer101@gmail.com>
2023-11-15 17:39:33 +0800

    DevOps jobs at 2023/11/15

---
+++ seek/README.md
@@ -0,0 +1,40 @@
+# About the script
+
+This script allows you to search for corresponding positions on the seek website by definition and save it to a csv file.
+
+## Features
+
+- Define search keywords
+- Search for corresponding positions and save them to csv file
+
+## Prerequisites
+
+- Python 3.6.X installed on your system.
+
+## Setup
+
+1. Install python dependency packages
+
+```
+pip3 install requests
+pip3 install selenium
+pip3 install beautifulsoup4
+```
+
+2. Install ChromeDriver and Chrome Browser with specified version
+
+```
+visit https://googlechromelabs.github.io/chrome-for-testing/ to download Chrome driver and Chrome browser, then place them in the following directories and unzip them.
+/Users/lxdluser01/
+```
+
+## Usage
+
+1. Replace the KeyWord variable in the script with the keyword you want to search for.
+2. Run the script using the command:
+
+```
+python seek/save_jobs.py
+```
+
+## Warning
--- seek/save_jobs.py
+++ seek/save_jobs.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import pandas as pd
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+
+# 搜索关键字
+KeyWord = 'DevOps'
 
 # Chromedriver路径
 chrome_driver_path = r'/Users/lxdluser01/chromedriver-mac-x64/chromedriver'
 # Chrome浏览器路径
-# chrome_binary_path = r'/Users/lxdluser01/chrome-mac-x64/Google Chrome for Testing.app'
 chrome_binary_path = r'/Users/lxdluser01/chrome-mac-x64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing'
-# chrome_binary_path = r'/Users/lxdluser01/chrome-mac-x64/Google\ Chrome\ for\ Testing.app/Contents/MacOS/'
 
 # 创建Chrome浏览器选项
 chrome_options = Options()
@@ -19,61 +21,54 @@ chrome_options.add_argument("--headless")  # 无头模式
 chrome_options.add_argument("--disable-gpu")  # 禁用GPU加速
 chrome_options.add_argument("--window-size=1920x1080")  # 设置窗口大小
 chrome_options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")  # 设置用户代理
-
 # 启动Chrome浏览器
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# 打开页面
-driver.get('https://www.seek.com.au/DevOps-jobs/in-All-Australia')
+# 定义csv文件名
+CSV_FILE = KeyWord + '_' + 'jobs_inAUS' + '_' + datetime.now().strftime("%Y-%m-%d-%H-%M")+".csv"
 
-# 等待页面加载
-driver.implicitly_wait(10)
+# 从第一页开始遍历
+page_num = 1
+while True:
+    # 拼接url
+    url = 'https://www.seek.com.au/' + KeyWord + '-jobs/in-All-Australia' + '?page=' + str(page_num)
+    print("Page Number: " + str(page_num))
+    driver.get(url)
+    driver.implicitly_wait(10)
+    rendered_page_source = driver.page_source
+    if "No matching search results" in rendered_page_source:
+        break
+    # 获取渲染后的页面内容
+    rendered_page_source = driver.page_source
+    soup = BeautifulSoup(rendered_page_source, 'html.parser')
 
-# 获取渲染后的页面内容
-rendered_page_source = driver.page_source
-#分割线
-print("########################1")
-# 将渲染后的页面内容写入到txt文件中
-with open('rendered_page.txt', 'w', encoding='utf-8') as file:
-    file.write(rendered_page_source)
-# 打印渲染后的页面内容
-print(rendered_page_source)
+    # 找出所有data-card-type为"JobCard"的元素
+    job_cards = soup.find_all('article', attrs={'data-card-type': 'JobCard'})
+    # 创建CSV文件
+    with open(CSV_FILE, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # 如果文件不存在或为空，写入表头
+        if file.tell() == 0:
+            writer.writerow(["Job Title", "Company", "Location", "Salary", "Write Time", "Release Time"])
 
-#分割线
-print("########################2")
+        # 遍历所有的job cards
+        for job_card in job_cards:
+            # 找出data-automation分别为jobTitle、jobCompany、jobLocation、jobSalary的元素
+            job_title = job_card.find(attrs={'data-automation': 'jobTitle'}).text if job_card.find(attrs={'data-automation': 'jobTitle'}) else "N/A"
+            job_company = job_card.find(attrs={'data-automation': 'jobCompany'}).text if job_card.find(attrs={'data-automation': 'jobCompany'}) else "N/A"
+            job_location = job_card.find(attrs={'data-automation': 'jobLocation'}).text if job_card.find(attrs={'data-automation': 'jobLocation'}) else "N/A"
+            job_salary = job_card.find(attrs={'data-automation': 'jobSalary'}).text if job_card.find(attrs={'data-automation': 'jobSalary'}) else "N/A"
 
-# 找出所有包含所需信息的职位列表项
-# job_listings = driver.find_elements(By.CSS_SELECTOR, '[data-card-type="JobCard"]')
-job_listings = driver.find_elements(By.NAME, 'JobCard')
+            # 获取当前时间作为写入时间
+            write_time = datetime.now().strftime("%Y/%m/%d %H:%M")
 
-
-# 打印job_listings列表
-print("########################22")
-print(job_listings)
-
-# 创建空的列表来保存职位信息
-job_data = []
-
-
-# 遍历每个职位列表项并提取所需信息
-for job_listing in job_listings:
-    job_title = job_listing.find_element(By.NAME, 'jobTitle').text.strip()
-    job_company = job_listing.find_element(By.NAME, 'jobCompany').text.strip()
-    job_location = job_listing.find_element(By.NAME, 'jobLocation').text.strip()
-    job_salary = job_listing.find_element(By.NAME, 'jobSalary').text.strip()
-    print('jobTitle: ', job_title, ", jobCompany: ", job_company, ", jobLocation: ", job_location ,", jobSalary: ", job_salary)
-    # 将职位信息添加到列表中
-    job_data.append([job_title, job_company, job_location, job_salary])
+            # 获取发布时间
+            job_listing_date = job_card.find(attrs={'data-automation': 'jobListingDate'})
+            release_time = job_listing_date.text if job_listing_date else "Unknown"
+            
+            # 写入一行数据
+            writer.writerow([job_title, job_company, job_location, job_salary, write_time, release_time])
+    page_num += 1
     
-# 打印job_data列表
-print("########################3")
-print(job_data)
-
-# 创建DataFrame对象
-df = pd.DataFrame(job_data, columns=["Job Title", "Company", "Location", "Salary"])
-
-# 将数据保存到CSV文件
-df.to_csv("job_data.csv", index=False)
-
 # 关闭浏览器
 driver.quit()
